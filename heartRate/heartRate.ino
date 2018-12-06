df9dd4d764643ab39624dcd6917f01cd4c8c2f87
Aurexolf <jackedgill@gmail.com>
2018-12-06 19:07:29 +0000

    Adjusted variables and added error checking

--- heartRate/heartRate.ino
+++ heartRate/heartRate.ino
@@ -3,15 +3,15 @@
 
 MAX30105 particleSensor;
 
-const byte BUFFER_SIZE = 200;
+const float correlationThreshhold = 0.8;
 
-int16_t irBuffer[BUFFER_LENGTH]; //infrared LED sensor data
-int16_t redBuffer[BUFFER_LENGTH];  //red LED sensor data
+uint16_t irBuffer[BUFFER_LENGTH]; //infrared LED sensor data
+uint16_t redBuffer[BUFFER_LENGTH];  //red LED sensor data
 
 int16_t spo2; //SPO2 value
 boolean validSPO2; //indicator to show if the SPO2 calculation is valid
 double heartRate; //heart rate value
-byte validHeartRate; //indicator to show if the heart rate calculation is valid
+boolean validHeartRate; //indicator to show if the heart rate calculation is valid
 
 //Initalise the MAX chip
 void setup() {
@@ -20,19 +20,19 @@ void setup() {
   // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
-    //Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
     while (1);
   }
 
   Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-  //while (Serial.available() == 0) ; //wait until user presses a key
-  //Serial.read();
+  while (Serial.available() == 0) ; //wait until user presses a key
+  Serial.read();
 
-  byte ledBrightness = 20; //Options: 0=Off to 255=50mA
-  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
+  byte ledBrightness = 40; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 2; //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  int sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 69; //Options: 69, 118, 215, 411
+  int sampleRate = 50; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
   int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
@@ -48,8 +48,8 @@ void loop() {
     while (particleSensor.available() == false) //do we have new data?
       particleSensor.check(); //Check the sensor for new data
 
-    redBuffer[i] = (int16_t) particleSensor.getRed();
-    irBuffer[i] = (int16_t) particleSensor.getIR();
+    redBuffer[i] = (uint16_t) particleSensor.getRed();
+    irBuffer[i] = (uint16_t) particleSensor.getIR();
     particleSensor.nextSample(); //We're finished with this sample so move to next sample
 
     Serial.print(F("Red = "));
@@ -61,8 +61,8 @@ void loop() {
 
   Serial.println(F("Initial Values obtained"));
 
-  
-  calculate_heart(irBuffer, BUFFER_SIZE, &heartRate, &validHeartRate);
+  if(checkCorrellation(irBuffer, redBuffer) > correlationThreshhold)
+  calculate_heart(irBuffer, &heartRate, &validHeartRate);
 
   //Continuously taking samples from MAX30102
   while (1)
@@ -86,7 +86,7 @@ void loop() {
     }
 
     //After gathering 25 new samples recalculate HR and SP02
-    calculate_heart(irBuffer, BUFFER_SIZE, &heartRate, &validHeartRate);
+    calculate_heart(irBuffer, &heartRate, &validHeartRate);
 
     if(validHeartRate){
       Serial.print(F("HR = "));
